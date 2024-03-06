ab44d002f2eef0caceacd7e980b2a17c35190e95
David <daalvarez@birt.eus>
2024-03-06 21:36:03 +0100

    funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,3 @@
-import { environment } from 'src/environments/environment';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import jsPDF from 'jspdf';
 
@@ -7,13 +6,9 @@ import jsPDF from 'jspdf';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-  @ViewChild('contaniner') contaniner !:ElementRef;
-  
+export class HomePage {
+   @ViewChild('container') container!: ElementRef;
   constructor() {}
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
   
   generarPDF() {
     //Ancho en px de A4
@@ -27,8 +22,8 @@ export class HomePage implements OnInit{
       format: [anchoMax,altoMax]
     });
     // Agregar el contenido HTML al PDF
-    const content = this.contaniner.nativeElement.querySelector('.dasboard-content') as HTMLElement
-    
+    const content = this.container.nativeElement.querySelector('.dashboard-content') as HTMLElement;
+
     content.style.maxWidth = anchoMax+"px";
     //const content = document.getElementById('container') as HTMLElement;
     doc.html(content, {
@@ -39,5 +34,6 @@ export class HomePage implements OnInit{
       }
     });
   }
-
+  
+  
 }
